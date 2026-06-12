47a96438fa4905af9a60c011d6dc8e2cc13334f3
Berke Atalay <98462442+berkeatalay1@users.noreply.github.com>
2026-06-11 21:49:19 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        myHash = {}
+
+        for i in s:
+            myHash[i] = myHash.get(i, 0) + 1
+
+        for i in t:
+            if i not in myHash:
+                return False
+            else:
+                myHash[i] -= 1
+        
+        
+        for k,v in myHash.items():
+            if v != 0:
+                return False
+        
+        return True
